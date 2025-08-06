405e083eeb3ae2bee37401933d371566d49ecdb1
Mohan rao <gedalamohanraolpu@gmail.com>
2025-08-06 11:27:57 +0530

    day 1

--- Week1/intro.py
+++ Week1/intro.py
@@ -1,6 +1,44 @@
+import keyword
+
 # name=input("enter your namme : ")
 # print("your name is :"+name)
 
 
-x, y=input("enter two numbers:").split()
-print("first on ${:.2f} and second one ${:.2f}".format(x,y));
+# x, y=input("enter two numbers:").split()
+# print("first on ${:.2f} and second one ${:.2f}".format(x,y))
+
+
+# # end Parameter with '@'
+# print("Python", end='@')
+# print("GeeksforGeeks")
+
+# # Seprating with Comma
+# print('G', 'F', 'G', sep='')
+
+# # for formatting a date
+# print('09', '12', '2016', sep='-')
+
+# # another example
+# print('pratik', 'geeksforgeeks', sep='@')
+
+
+# name = 'Tushar'
+# age = 23
+# print(f"Hello, My name is {name} and I'm {age} years old.")
+
+
+# # Taking input from the user
+# num = int(input("Enter a value: "))
+
+# add = num + 5
+
+# # Output
+# print("The sum is %d" %add)
+
+
+# print("The list of keywords is : ")
+# print(keyword.kwlist)
+
+
+name="venkey"
+print(name*2)
